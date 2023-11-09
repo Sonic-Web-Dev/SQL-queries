67a3ab270b4e739006befbd0864512082f3b66e4
Jennifer Nolasco <jennifer@sonicwebdev.com>
2023-11-09 15:13:45 -0500

    Added a comment

--- leadFlowByDay.sql
+++ leadFlowByDay.sql
@@ -5,6 +5,8 @@ FROM (SELECT DATE_TRUNC('day', "created") AS "date", "vendorID", "layerID",
 
 FROM public.leads_prod lp
 
+/* replace "{{ networkid }}" with the actual networkID(s) wrapped in single quotes,
+   separated by a comma if there are multiple networks you want to filter for. */
 WHERE lp."networkID" IN ({{ networkid }})
   AND "leadResult" = 'Accepted'
   AND DATE_TRUNC('day', "created") >= DATE(NOW()) - INTERVAL '45 days'
