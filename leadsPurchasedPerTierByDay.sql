03087c9dd381ca0103ffb7290f45e7844d1a26fd
Jennifer Nolasco <jennifer@sonicwebdev.com>
2023-11-09 15:42:46 -0500

    Created and wrote the initial query

---
+++ leadsPurchasedPerTierByDay.sql
@@ -0,0 +1,20 @@
+SELECT *
+
+FROM (SELECT DATE_TRUNC('day', "created") AS "date", "name", "description", "layerID",
+       COUNT(*) AS "count"
+
+FROM public.leads_prod lp
+  INNER JOIN public.leads_sold_prod lsp
+    ON lp."leadID" = lsp."leadID"
+  INNER JOIN public.price_tiers_prod ptp
+    ON lsp."tierID" = ptp."priceTierID"
+
+/* replace "{{ networkid }}" with the actual networkID(s) wrapped in single quotes,
+   separated by a comma if there are multiple networks you want to filter for. */
+WHERE lp."networkID" IN ({{ networkid }})
+  AND "leadResult" = 'Accepted'
+  AND DATE_TRUNC('day', "created") >= DATE(NOW()) - INTERVAL '45 days'
+
+GROUP BY "date", "name", "description", "layerID") q
+
+WHERE "count" > 0;
