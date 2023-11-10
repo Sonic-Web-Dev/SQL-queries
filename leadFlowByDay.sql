2988b02871eacc5aee7002d2db0b1f61dc014eed
Jennifer Nolasco <jennifer@sonicwebdev.com>
2023-11-10 11:37:52 -0500

    Added a comment

--- leadFlowByDay.sql
+++ leadFlowByDay.sql
@@ -7,6 +7,7 @@ FROM public.leads_prod lp
 
 /* replace "{{ networkid }}" with the actual networkID(s) wrapped in single quotes,
    separated by a comma if there are multiple networks you want to filter for. */
+-- test
 WHERE lp."networkID" IN ({{ networkid }})
   AND "leadResult" = 'Accepted'
   AND DATE_TRUNC('day', "created") >= DATE(NOW()) - INTERVAL '45 days'
