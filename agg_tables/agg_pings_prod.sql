643c35356f6d930e9ed2313600ba75da67cf2f67
Jennifer Nolasco <jennifer@sonicwebdev.com>
2023-11-10 16:04:52 -0500

    Added date function and layerID

--- agg_tables/agg_pings_prod.sql
+++ agg_tables/agg_pings_prod.sql
@@ -1,4 +1,4 @@
-SELECT p."networkID", DATE_TRUNC('hour', p."created") AS "date", p."vendorID", p."mpVendorCampaignID", p."resultMessage", COUNT(p."resultMessage") AS "Ping Count",
+SELECT p."networkID", DATE_TRUNC('hour', p."created") AS "date", p."vendorID", p."layerID", p."mpVendorCampaignID", p."resultMessage", COUNT(p."resultMessage") AS "Ping Count",
   SUM(CASE WHEN lp."leadID" IS NOT NULL AND lp."leadResult" = 'Accepted' THEN 1 ELSE 0 END) AS "Leads Won",
   AVG(CASE WHEN p."bid" IS NOT NULL THEN p."bid"/100 ELSE 0 END) AS "AVG Bid", ROUND(AVG(CASE WHEN p."ageInt" > 17 AND p."ageInt" < 101 THEN p."ageInt" END)) AS "AVG Age",
   ROUND(AVG(CASE WHEN p."householdInt" >= 0 AND p."householdInt" <= 10 THEN p."householdInt" END)) AS "AVG Household",
@@ -10,6 +10,6 @@ FROM public."pings_prod" p
   INNER JOIN public."vendor_campaigns_prod" vcp
     ON p."mpVendorCampaignID" = vcp."mpVendorCampaignID"
 
-WHERE p."created" >= '{start_date}' AND p."created" < '{end_date}'
+WHERE DATE(p."created") >= '{start_date}' AND DATE(p."created") < '{end_date}'
 
-GROUP BY p."networkID", DATE_TRUNC('hour', p."created"), p."vendorID", p."mpVendorCampaignID", p."resultMessage";
+GROUP BY p."networkID", DATE_TRUNC('hour', p."created"), p."vendorID", p."layerID", p."mpVendorCampaignID", p."resultMessage";
