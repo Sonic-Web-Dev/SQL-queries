e17e194a4b6d9cd315bc47341daf8709d19338b7
Jennifer Nolasco <jennifer@sonicwebdev.com>
2023-11-27 10:52:18 -0500

    Applied fix for leadResult mapping issue

--- sara_recons_blocked.sql
+++ sara_recons_blocked.sql
@@ -1,5 +1,7 @@
 CREATE TABLE sara_recons_blocked AS
-SELECT DATE_TRUNC('hour', lp."created") AS "createdDate", lp."networkID", "leadResult", lp."vendorID",
+SELECT DATE_TRUNC('hour', lp."created") AS "createdDate", lp."networkID",
+       CASE WHEN lp."requestResult" IS NOT NULL THEN lrx."leadResult" ELSE CAST(lp."leadResult" AS varchar(100)) END AS "leadResult",
+       lp."vendorID",
        COUNT(lp."leadID") AS "total blocked",
        SUM(CASE WHEN lpp."isOriginal" = true THEN 1 ELSE 0 END) AS "unique vendor",
        SUM(CASE WHEN lp."requestResult" = 'ushgGTB' THEN 1 ELSE 0 END) AS "gtb checks",
@@ -9,10 +11,12 @@ SELECT DATE_TRUNC('hour', lp."created") AS "createdDate", lp."networkID", "leadR
 FROM public.leads_prod lp
   LEFT JOIN public.leads_phone_prod lpp
     ON lp."leadID" = lpp."leadID"
+  LEFT JOIN public.lead_result_xref lrx
+    ON lp."requestResult" = lrx."requestResult"
 
 WHERE DATE_TRUNC('day', lp."created") >= '2023-01-01'
-  AND "leadType" != 'recycled'
-  AND "leadResult" != 'Accepted'
+  AND lp."leadType" != 'recycled'
+  AND lp."leadResult" != 'Accepted'
 
-GROUP BY "createdDate", lp."networkID", lp."leadResult", lp."vendorID"
+GROUP BY "createdDate", lp."networkID", lp."leadResult", lp."vendorID", lp."requestResult", lrx."leadResult"
 ORDER BY COUNT(lp."leadID") DESC;
