7893c9a519871aa75726de0a614422450a1013eb
Jennifer Nolasco <jennifer@sonicwebdev.com>
2023-12-05 15:44:35 -0500

    Added brokerID to the table

--- sara_recons.sql
+++ sara_recons.sql
@@ -1,6 +1,14 @@
 /* Table for Total sums of EACH Network's Counts (not including blocked) */
 CREATE MATERIALIZED VIEW sara_recons AS
-SELECT DATE_TRUNC('hour', lsp."soldDate") AS "soldDates", lp."networkID", lp."vendorID", lsp."agentID", ap."email", ptp."name" AS "tier", lp."leadType", ptp."priceTierID" AS "tierID",
+SELECT DATE_TRUNC('hour', lsp."soldDate") AS "soldDates",
+       lp."networkID",
+       np."brokerID",
+       lp."vendorID",
+       lsp."agentID",
+       ap."email",
+       ptp."name" AS "tier",
+       lp."leadType",
+       ptp."priceTierID" AS "tierID",
        COUNT(lsp."leadID") AS "lead count",
        SUM(CASE WHEN lsp."price" IS NOT NULL THEN lsp."price" ELSE 0 END) * 0.01 AS "agent spend",
        SUM(CASE WHEN lp."buyerPrice" IS NOT NULL THEN lp."buyerPrice" ELSE 0 END) * 0.01 AS "vendor costs"
@@ -12,8 +20,10 @@ FROM public.leads_prod lp
     ON lsp."agentID" = ap."agentID"
   INNER JOIN public.price_tiers_prod ptp
     ON lsp."tierID" = ptp."priceTierID"
+  LEFT JOIN public.networks_prod np
+    ON lp."networkID" = np."networkID"
 
 WHERE lsp."soldDate" >= '2023-01-01'
 
-GROUP BY "soldDates", lp."networkID", lp."vendorID", lsp."agentID", ap."email", "tier", lp."leadType", ptp."priceTierID"
+GROUP BY "soldDates", lp."networkID", np."brokerID", lp."vendorID", lsp."agentID", ap."email", "tier", lp."leadType", ptp."priceTierID"
 ORDER BY COUNT(lsp."leadID") DESC;
